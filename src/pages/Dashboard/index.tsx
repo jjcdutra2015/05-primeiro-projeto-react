b4ba5bf67df28f56f26a8e095ea8d750ac678cfc
Julio Dutra <jjcdutra@gmail.com>
2020-05-08 20:58:52 -0300

    Estilizando Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,60 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github.</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/15237170?s=60&u=9422d7316417ce48d8b0db0a5ef431dde45704f0&v=4"
+            alt="Julio Dutra"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/15237170?s=60&u=9422d7316417ce48d8b0db0a5ef431dde45704f0&v=4"
+            alt="Julio Dutra"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/15237170?s=60&u=9422d7316417ce48d8b0db0a5ef431dde45704f0&v=4"
+            alt="Julio Dutra"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
